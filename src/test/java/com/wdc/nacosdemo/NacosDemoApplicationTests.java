16efc395b46fc264ea5443880e61111e6c2a3dba
YWang665 <ywang665@its.jnj.com>
2024-11-04 17:30:14 +0800

    init project

---
+++ src/test/java/com/wdc/nacosdemo/NacosDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdc.nacosdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
